838d53e0936d57bb289f101385f31e6c86cc4629
FGabriel0 <gabrielfabio3g@gmail.com>
2023-04-19 18:19:13 -0300

    Iniciando Portifolio

---
+++ js/script.js
@@ -0,0 +1,6 @@
+//DashBoard
+let hamburguer = document.querySelector(".hamburguer");
+
+hamburguer.addEventListener("click", () => {
+    let sidebar = document.querySelector(".sidebar").classList.toggle("show-menu");
+});
