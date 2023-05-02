c272ac88f2899ca2ae1f17ce3268350917e16a39
FGabriel0 <gabrielfabio3g@gmail.com>
2023-05-02 11:26:13 -0300

    Atualizado

--- js/script.js
+++ js/script.js
@@ -8,6 +8,24 @@ hamburguer.addEventListener("click", () => {
 });
 
 
+function reveal(){
+  let reveals = document.querySelectorAll(".reveal");
+
+  reveals.forEach((reveal) =>{
+    let windowHeight = window.innerHeight;
+    let elementTop =reveal.getBoundingClientRect().top;
+    let elementvisible = 100;
+
+    if(elementTop < windowHeight - elementvisible){
+      reveal.classList.add("active");
+  
+    }else{
+      reveal.classList.remove("active");
+    }
+  })
+}
+
+window.addEventListener("scroll", reveal);
 
 
 
