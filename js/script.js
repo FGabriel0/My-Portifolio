b88f8dcb4f57b91269e7f443d58eb197f9c17da1
FGabriel0 <gabrielfabio3g@gmail.com>
2023-05-17 08:37:16 -0300

    Criação da área de Skills

--- js/script.js
+++ js/script.js
@@ -8,6 +8,36 @@ hamburguer.addEventListener("click", () => {
 });
 
 
+//DarkMode
+const changeThemeBtn = document.querySelector("#change-theme");
+
+function toggleDarkMode() {
+  document.body.classList.toggle("dark");
+}
+
+function loadTheme() {
+  const darkMode = localStorage.getItem("dark");
+
+  if (darkMode) {
+    toggleDarkMode();
+  }
+}
+
+loadTheme();
+
+changeThemeBtn.addEventListener("change",() => {
+  toggleDarkMode();
+
+  // Save or remove dark mode from localStorage
+  localStorage.removeItem("dark");
+
+  if (document.body.classList.contains("dark")) {
+    localStorage.setItem("dark", 1);
+  }
+});
+
+
+//ReavelPage
 function reveal(){
   let reveals = document.querySelectorAll(".reveal");
 
